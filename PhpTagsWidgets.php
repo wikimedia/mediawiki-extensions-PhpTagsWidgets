495064b2242762832a4bef7c89d3d7efa4f28808
Pavel Astakhov <pastakhov@yandex.ru>
2014-12-02 11:07:00 +0600

    update Slick library to 1.3.15 (v 1.3.2)
    Change-Id: I6ecb516ca7c0be878f43215096756a8f874f4213

--- PhpTagsWidgets.php
+++ PhpTagsWidgets.php
@@ -33,7 +33,7 @@ if ( PHPTAGS_HOOK_RELEASE != 5 ) {
 	);
 }
 
-define( 'PHPTAGS_WIDGETS_VERSION' , '1.3.1' );
+define( 'PHPTAGS_WIDGETS_VERSION' , '1.3.2' );
 
 // Register this extension on Special:Version
 $wgExtensionCredits['phptags'][] = array(
