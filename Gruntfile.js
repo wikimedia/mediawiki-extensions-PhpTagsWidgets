83074f60c8d94fdf0eb720f977695d43a0d6754a
Antoine Musso <hashar@free.fr>
2017-04-21 17:59:57 +0200

    build: add grunt-contrib-jshint
    See T119973
    
    Change-Id: I933d02bf4361f647d8a0a96ace2dce6f5068d306

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,15 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
 		banana: {
 			all: 'i18n/'
+		},
+		jshint: {
+			all: '.'
 		},
 		jsonlint: {
 			all: [
@@ -16,6 +20,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'jshint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
