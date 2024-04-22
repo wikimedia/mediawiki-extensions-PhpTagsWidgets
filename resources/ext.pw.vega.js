f5e9ce4842954bdc6713b46741ad345f1a93cb7d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-04-22 04:14:45 +0000

    build: Updating dependencies
    composer:
    * php-parallel-lint/php-parallel-lint: 1.3.2 → 1.4.0
    
    npm:
    * eslint-config-wikimedia: 0.25.0 → 0.27.0
    * grunt-banana-checker: 0.11.0 → 0.11.1
    * grunt-eslint: 24.0.1 → 24.3.0
    
    Change-Id: Ic22086cc5ad5521318fe6103240ca6217ed3ddd7

--- resources/ext.pw.vega.js
+++ resources/ext.pw.vega.js
@@ -10,7 +10,9 @@
 		vg.config.safeMode = true;
 		vg.config.domainWhiteList = mw.config.get( 'ext.phptags.Widgets' ).vega.domainWhiteList;
 		this.each( function ( index, element ) {
-			vg.parse.spec( $data, function ( chart ) { chart( { el: element } ).update(); } );
+			vg.parse.spec( $data, function ( chart ) {
+				chart( { el: element } ).update();
+			} );
 		} );
 	};
 
